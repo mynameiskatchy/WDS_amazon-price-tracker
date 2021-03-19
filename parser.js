bb39314d7e16696ff60401484b1fa239dc559f02
mynameiskatchy <karchietrinh@gmail.com>
2021-03-19 13:59:09 -0400

    Project set-up. For some reason using id variable instead of plaintext breaks .wait and .evaluate

---
+++ parser.js
@@ -0,0 +1,23 @@
+const nightmare = require('nightmare')() // use defaults
+
+const id = "priceblock_ourprice"
+const URL = "https://www.amazon.com/Samsung-enhanced-tracking-analysis-coaching/dp/B07VQJDGX2"
+
+checkPrice()
+
+async function checkPrice() {
+    try {
+        const priceString = await nightmare
+            .goto(URL)      
+            .wait("#priceblock_ourprice")       
+            .evaluate(() => document.getElementById("priceblock_ourprice").innerText)        
+            .end()
+        console.log(priceString)
+        const priceNumber = parseFloat(priceString.replace('$', ''))
+        console.log(priceNumber)
+    } catch (e) {
+        console.log('Amazon price error')
+        throw e
+    }
+}
+
