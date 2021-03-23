0237344c79e2b0596ec65d9cc7ff13a99bf0e1d0
mynameiskatchy <karchietrinh@gmail.com>
2021-03-23 01:43:58 -0400

    Add functionality to use with node args

--- parser.js
+++ parser.js
@@ -1,5 +1,9 @@
 const nightmare = require('nightmare')() // use defaults
 
+const args = process.argv.slice(2)
+const url = args[0]
+const minPrice = args[1]
+
 const id = "priceblock_ourprice"
 const URL = "https://www.amazon.com/Samsung-enhanced-tracking-analysis-coaching/dp/B07VQJDGX2"
 
@@ -8,14 +12,14 @@ checkPrice()
 async function checkPrice() {
     try {
         const priceString = await nightmare
-            .goto(URL)      
+            .goto(url)      
             .wait("#priceblock_ourprice")       
             .evaluate(() => document.getElementById("priceblock_ourprice").innerText)        
             .end()
         console.log(priceString)
         const priceNumber = parseFloat(priceString.replace('$', ''))
         console.log(priceNumber)
-        if (priceNumber < 150) {
+        if (priceNumber < minPrice) {
             console.log("It is cheap")
         } else {
             console.log("It is expensive")
